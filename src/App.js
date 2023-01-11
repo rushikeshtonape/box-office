12cef4e0949ad802980f7675593039a447402e2d
rushikeshtonape <rushikeshtonape143@gmail.com>
2023-01-11 14:28:59 +0530

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
-  return <div>Hiii</div>;
+  return (
+    <Routes>
+      <Route path="/" element={"hello"}></Route>
+      <Route path="/Rushi" element={"THis is Rushis page"}></Route>
+      <Route path="/starred" element={"This is starred Page"}></Route>
+      <Route element={"This is 404 Page"}></Route>
+    </Routes>
+  );
 }
 
 export default App;
